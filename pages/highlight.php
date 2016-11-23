f5dff4750c99d4e40402fafb268b14a4e0da6676
mimaverde <verdemima@gmail.com>
2016-11-22 22:05:15 -0200

    Finalização da Home Desktop

--- pages/highlight.php
+++ pages/highlight.php
@@ -1,11 +1,11 @@
 <section id="home" class="home">
 	<h1 class="logo">Subjetivas</h1>
 	<video autoplay  poster="<?= get_template_directory_uri(); ?>/src/images/poster.jpg" id="videobg" loop>
-		<!-- 
-		<source src="src/videos/subjetivas.mp4" type="video/mp4">
-		<source src="src/videos/subjetivas.ogv" type="video/ogv">
-		<source src="src/videos/subjetivas.webm" type="video/webm"> 
-		-->
+		 
+		<source src="<?= get_template_directory_uri(); ?>/src/videos/subjetivas.mp4" type="video/mp4">
+		<source src="<?= get_template_directory_uri(); ?>/src/videos/subjetivas.ogv" type="video/ogv">
+		<source src="<?= get_template_directory_uri(); ?>/src/videos/subjetivas.webm" type="video/webm"> 
+		
 		Your browser does not support the video tag. I suggest you upgrade your browser.
 	</video>
 </section>
