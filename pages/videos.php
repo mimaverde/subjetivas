e80e0fd2b6ac2961e6b55ad5aa1682a5dd29b56b
mimaverde <verdemima@gmail.com>
2016-11-30 00:05:45 -0200

    listagem de vídeos

--- pages/videos.php
+++ pages/videos.php
@@ -7,28 +7,38 @@
 	<!-- Busca por Estado -->
 	<?php $taxonomias_estado = get_terms('estado'); ?>
 	<?php $taxonomias_funcao = get_terms('funcao'); ?>
-	<form id="filter" action="<?php bloginfo('url'); ?>" method="GET">
-		<select class="select-estado" name="taxonomy_estado">
-			<option value="">Todos os Estados</option>
-			<?php foreach ($taxonomias_estado as $taxonomia) { ?>
-				<option value="<?= $taxonomia->slug; ?>"><?= $taxonomia->name; ?></option>
-			<?php } ?>
-		</select>
+	<div class="box-search">
+		<form id="filter" action="<?php bloginfo('url'); ?>" method="GET">
+			<div class="listar">Listar</div>
+			<ul class="menu-filtro">
+				<li><a href="#" class="filtro">Por Função</a>
+					<ul class="select-funcao sub-menu">
+						<li><a href="#" value="">Todos as função</a></li>
+						<?php foreach ($taxonomias_funcao as $taxonomia) { ?>
+							<li><a href="<?= $taxonomia->slug; ?>"><?= $taxonomia->name; ?></a></li>
+						<?php } ?>
+					</ul>
+				</li>
+				<li><a href="#" class="filtro">Por Estado</a>
+				 	<ul class="select-estado sub-menu">
+				 		<li><a href="#" value="">Todos os Estados</a></li>
+						<?php foreach ($taxonomias_estado as $taxonomia) { ?>
+							<li><a href="<?= $taxonomia->slug; ?>"><?= $taxonomia->name; ?></a></li>
+						<?php } ?>
+				 	</ul>
+				</li>
+				<li><a href="#" class="filtro">Por Data</a>
+					<ul class="sub-menu">
+						<li><a href="" class="dateSel" id="DESC">Recentes</a></li>
+						<li><a href="" class="dateSel" id="ASC">Antigos</a></li>
+					</ul>
+				</li>
+			</ul>
 
-		<select class="select-funcao" name="taxonomy_funcao">
-			<option value="">Todos as função</option>
-			<?php foreach ($taxonomias_funcao as $taxonomia) { ?>
-				<option value="<?= $taxonomia->slug; ?>"><?= $taxonomia->name; ?></option>
-			<?php } ?>
-		</select>
-		<div>
-			<a href="" class="dateSel" id="ASC">Antigos<a/>
-			<a href="" class="dateSel" id="DESC">Recentes<a/>
-		</div>
-
-		<input type="hidden" name="action" value="myfilter">
-		<?php get_search_form(); ?>
-	</form>
+			<input type="hidden" name="action" value="myfilter">
+			<?php get_search_form(); ?>
+		</form>
+	</div>
 
 	<!-- Galeria de vídeos -->
 	<div id="category-post-content">
@@ -55,7 +65,7 @@
 				);
 			}
 		?>
-
+		<ul class="gallery">
 		<?php
 			$args = array(
 				'post_type' => 'videos',
@@ -70,50 +80,54 @@
 				while($loop->have_posts()) {
 					$loop->the_post();
 		?>
-
-			<img src="<?php video_thumbnail(); ?>" />
-			<h2><?php the_title(); ?></h2>
-			
-			<div>
-				<div><?php the_content(); ?></div>
-				<?php $videos_meta_data = get_post_meta( $post->ID); ?>
-				<div>
-					<h2>Sobre a realizadora</h2>
-					<p>Nome:</p>
-					<p><?= $videos_meta_data['name_id'][0] ?></p>
-					<p>Cidade:</p>
-					<p><?= $videos_meta_data['city_id'][0] ?></p>
-					<p>Área de Atuação:</p>
-					<p></p>
-					<p>Biografia:</p>
-					<p><?= $videos_meta_data['biography_id'][0] ?></p>
+			<li>
+				<a href="#">
+					<img src="<?php video_thumbnail(); ?>" />
+					<h2><?php the_title(); ?></h2>
+				</a>
+				
+				<div class="box-desc">
+					<div><?php the_content(); ?></div>
+					<?php $videos_meta_data = get_post_meta( $post->ID); ?>
+					<div>
+						<h2>Sobre a realizadora</h2>
+						<p>Nome:</p>
+						<p><?= $videos_meta_data['name_id'][0] ?></p>
+						<p>Cidade:</p>
+						<p><?= $videos_meta_data['city_id'][0] ?></p>
+						<p>Área de Atuação:</p>
+						<p></p>
+						<p>Biografia:</p>
+						<p><?= $videos_meta_data['biography_id'][0] ?></p>
+					</div>
+					<div>
+						<h2>Trabalhos</h2>
+						<p>Links:</p>
+						<p><?= $videos_meta_data['links_id'][0] ?></p>
+					</div>
+					<div>
+						<h2>Contato</h2>
+						<p>E-mail:</p>
+						<p><a target="_blank" href="mailto=<?= $videos_meta_data['email_id'][0] ?>"><?= $videos_meta_data['email_id'][0] ?></a></p>
+						<p>Telefone:</p>
+						<p><?= $videos_meta_data['phone_id'][0] ?></p>
+						<p>Portfólio:</p>
+						<p><a target="_blank" href="<?= $videos_meta_data['portfolio_id'][0] ?>"><?= $videos_meta_data['portfolio_id'][0] ?></a></p>
+						<p>Redes Sociais</p>
+						<a target="_blank" href="<?= $videos_meta_data['facebook_id'][0] ?>"></a>">Facebook</a>
+						<a target="_blank" href="<?= $videos_meta_data['instagram_id'][0] ?>">Instagram</a>
+						<a target="_blank" href="<?= $videos_meta_data['vimeo_id'][0] ?>">Vimeo</a>
+						<a target="_blank" href="<?= $videos_meta_data['youtube_id'][0] ?>">Youtube</a>
+					</div>
 				</div>
-				<div>
-					<h2>Trabalhos</h2>
-					<p>Links:</p>
-					<p><?= $videos_meta_data['links_id'][0] ?></p>
-				</div>
-				<div>
-					<h2>Contato</h2>
-					<p>E-mail:</p>
-					<p><a target="_blank" href="mailto=<?= $videos_meta_data['email_id'][0] ?>"><?= $videos_meta_data['email_id'][0] ?></a></p>
-					<p>Telefone:</p>
-					<p><?= $videos_meta_data['phone_id'][0] ?></p>
-					<p>Portfólio:</p>
-					<p><a target="_blank" href="<?= $videos_meta_data['portfolio_id'][0] ?>"><?= $videos_meta_data['portfolio_id'][0] ?></a></p>
-					<p>Redes Sociais</p>
-					<a target="_blank" href="<?= $videos_meta_data['facebook_id'][0] ?>"></a>">Facebook</a>
-					<a target="_blank" href="<?= $videos_meta_data['instagram_id'][0] ?>">Instagram</a>
-					<a target="_blank" href="<?= $videos_meta_data['vimeo_id'][0] ?>">Vimeo</a>
-					<a target="_blank" href="<?= $videos_meta_data['youtube_id'][0] ?>">Youtube</a>
-				</div>
-			</div>
+			</li>
 			
 		
 
-	<?php
+		<?php
+				}
 			}
-		}
-	?>
+		?>
+		</ul>
 	</div>
 </section>
--- searchform.php
+++ searchform.php
@@ -1,10 +1,6 @@
-/**
-Template Name: Search Form
-**/
-
 <form action="/" method="get" accept-charset="utf-8" id="searchform" role="search">
-  <div>
-    <input type="text" name="s" id="s" value="<?php the_search_query(); ?>" />
-    <input type="submit" id="searchsubmit" value="Search" />
+  <div class="box-busca">
+    <input type="text" name="s" id="s" value="<?php the_search_query(); ?>" class="inputBusca" placeholder="Buscar" />
+    <input type="submit" id="searchsubmit" class="buttonBusca" value="b" />
   </div>
 </form>
